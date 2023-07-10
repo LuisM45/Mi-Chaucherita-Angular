ca35bba5bfe96a045f53d790685e66ccca93a62e
LuisM45 <luismateoarg2012@gmail.com>
2023-07-10 18:01:29 -0500

    profile settings layout

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -3,16 +3,18 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { MaterialsModule } from '../materials/materials.module';
+import { ProfileSettingsComponent } from './profile-settings/profile-settings.component';
 
 
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, ProfileSettingsComponent],
   imports: [
     CommonModule,
     MaterialsModule,
     RouterModule.forChild([
-      { path:"login", component:LoginComponent}
+      { path:"login", component:LoginComponent},
+      { path:"profile", component:ProfileSettingsComponent}
     ])
   ]
 })
